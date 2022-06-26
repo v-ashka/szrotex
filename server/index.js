44e082dc0eac808da11d0504fd643e0ff15bf71a
Romek <mwijaszka99@gmail.com>
2022-06-26 23:20:37 +0200

    deploy

--- server/index.js
+++ server/index.js
@@ -44,8 +44,8 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "/frontend/build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
         console.log(__dirname);
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     }
     );
 }
