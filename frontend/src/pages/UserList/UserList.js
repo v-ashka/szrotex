1c14dce45733408f3a07881c44be5f0b215292a5
Romek <mwijaszka99@gmail.com>
2022-06-27 00:05:34 +0200

    deploy

--- frontend/src/pages/UserList/UserList.js
+++ frontend/src/pages/UserList/UserList.js
@@ -98,7 +98,7 @@ const App = ({value, isClicked, checkedVoivode, category}) => {
     // console.log(list)
     //Fetch List
     const fetchList = async () => {
-        const res = await fetch('http://localhost:3500/list')
+        const res = await fetch('http://localhost:3500/lists')
         const data = await res.json()
         //  console.log(data);
         const productList= [];
