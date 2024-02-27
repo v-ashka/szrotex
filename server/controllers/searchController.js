719bd551fc5d8257a9b8932c979ae4484c811cb2
vashka <mwijaszka99@gmail.com>
2024-02-27 14:05:29 +0100

    added search controller

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -7,34 +7,65 @@ const Product = require('../models/product.model')
 const searchProduct = async (req, res) => {
     
     // search queries
-    const { name, category, price } = req.body
+    const { name, category, minPrice, maxPrice } = req.body
     // Get all products from MongoDB
+    const filter = {}
+    // user.firstName = firstName || user.firstName
+    if(name){
+        // filter.name = { $regex: new RegExp(name, "i") }
+        filter.$or = [
+            { "name": { $regex: new RegExp(name, "i") } },
+            { "description": { $regex: new RegExp(name, "i") } },
+        ]
+        // filter.description = {$regex: new RegExp(name, "i")}
+    }
     
-    let products
-    if(!category){
-        products = await Product.find({
-                  $or: [
-                    { "name": { $regex: new RegExp(name, "i") } },
-                    { "description": { $regex: new RegExp(name, "i") } },
-                  ],
-          }).exec()
-    }else{
-        products = await Product.find({
-            $and: [
-                { "category" : {$in: category} },
-                {
-                    $or: [
-                    { "name": { $regex: new RegExp(name, "i") } },
-                    { "description": { $regex: new RegExp(name, "i") } },
-                  ],
-                }
-            ]
-            
-    }).exec()
-
+    if(category){
+        filter.category =  {$in: category }
     }
 
-    res.json(products)
+    if(minPrice && maxPrice){
+        filter.price =  { $gte: minPrice, $lte: maxPrice }
+    }
+
+    if(minPrice){
+        filter.price = { $gte: minPrice}
+    }
+
+    if(maxPrice){
+        filter.price = { $lte: maxPrice}
+    }
+    
+    
+
+    console.log(filter)
+    const searchResults = await Product.find(filter).exec()
+
+
+    // // if(!category){
+    // //     products = await Product.find({
+    // //               $or: [
+    // //                 { "name": },
+    // //                 { "description": { $regex: new RegExp(name, "i") } },
+    // //               ],
+    // //       }).exec()
+    // // }else{
+    // //     products = await Product.find({
+    // //         $and: [
+    // //             { "category" : {$in: category} },
+    // //             {
+    // //                 $or: [
+    // //                 { "name": { $regex: new RegExp(name, "i") } },
+    // //                 { "description": { $regex: new RegExp(name, "i") } },
+    // //               ],
+    // //             }
+    // //         ]
+            
+    // // }).exec()
+
+    // }
+
+    res.json(searchResults)
 }
 
 
